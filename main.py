f4a651972e480dad612877e4e89a386d6a1c4cfa
AlexeyProg <sokolov-lesha@yandex.ru>
2022-01-28 17:12:41 +0300

    complete translater. Добавил проверку, если не найдут слово

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import requests
 
 URL_AUTH = "https://developers.lingvolive.com/api/v1.1/authenticate"
 URL_TRANSLATE = "https://developers.lingvolive.com/api/v1/Minicard"
-KEY = "OWFlNjZjYjEtNzE2OS00ZTA4LWJlMjEtMjBmMWQ1Yzk1MDRjOmMxZmY5MDMwMjcxNDRiNDdiZDc1MzdmMWJiYzczYmM3"
-TOKEN = '5250513223:AAE7roliwVV11F9XvEVeIZyixmJmUZBW7Tc'
+KEY = ""
+TOKEN = ''
+text_help = "Для перевода слова с английского языка на русский просто введите необходимое слово"
 
 headers_auth = {"Authorization": "Basic " +KEY}
 auth = requests.post(URL_AUTH, headers=headers_auth)
@@ -17,16 +18,23 @@ if auth.status_code == 200:
 @bot.message_handler(content_types=["text"])
 def test(message):
     word = message.text
-    headers_translate = {
+    if word == "/help":
+        bot.send_message(message.chat.id, text_help)
+    else:
+        headers_translate = {
         "Authorization": "Bearer " + token
-    }
-    params = {
+        }
+        params = {
         "text": word,
         "srcLang": 1033,
         "dstLang": 1049
-    }
-    r = requests.get(URL_TRANSLATE, headers=headers_translate, params=params)
-    res = r.json()
-    bot.send_message(message.chat.id,res["Translation"] ["Translation"])
+        }
+        r = requests.get(URL_TRANSLATE, headers=headers_translate, params=params)
+        res = r.json()
+        try:
+            tex = res["Translation"] ["Translation"]
+        except:
+            tex = "Не удалось найти такое слово"
+        bot.send_message(message.chat.id, tex)
 
 bot.polling(none_stop=True)
